24b04dc8e3c692eba6e4c39d2040be696882fcfa
Anashwar23 <Anashwar.Saju@gisma-student.com>
2026-04-05 17:35:20 +0200

    Add files via upload

---
+++ Conditional_filtering_data_retrieval.sql
@@ -0,0 +1,15 @@
+#Orders greater than 100
+select * 
+from Orders 
+where total_amount > 100;
+
+# Orders greater than 100, sorted highest first
+select *
+from Orders
+where total_amount > 100
+order by total_amount desc;
+
+#Orders within a specific range
+select *
+from Orders
+where total_amount between 100 and 500;
