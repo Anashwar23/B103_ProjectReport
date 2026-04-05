efec0ebe7d206f6193b3c5d21d36b1a01d30a2be
Anashwar23 <Anashwar.Saju@gisma-student.com>
2026-04-05 17:23:41 +0200

    Add files via upload

---
+++ Data_Bulk_Insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO Customers (first_name, last_name, email, phone_number, 
+registration_date, country) 
+
+VALUES 
+('Alice', 'Brown', 'aliceb@example.com', '1111111111', '2024-02-10', 'UK'), 
+('Bob', 'Smith', 'bobsmith@example.com', '2222222222', '2024-03-25', 'Canada'), 
+('Charlie', 'Davis', 'charlied@example.com', '3333333333', '2024-05-30', 'Australia'); 
